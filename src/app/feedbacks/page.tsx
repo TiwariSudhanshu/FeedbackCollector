3b6822888a5abe2af18698397f58f1c6407b4924
Sudhanshu Tiwari <sudhanshutiwari9836@gmail.com>
2025-04-09 00:00:17 +0530

    Link Update

--- src/app/feedbacks/page.tsx
+++ src/app/feedbacks/page.tsx
@@ -1,5 +1,5 @@
 async function getFeedbacks() {
-    const res = await fetch("http:localhost:3000/api/feedback", {cache: "no-store"});
+    const res = await fetch("/api/feedback", {cache: "no-store"});
     if (!res.ok) {
       throw new Error("Failed to fetch data");
     }
